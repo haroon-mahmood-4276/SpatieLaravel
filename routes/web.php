77e73d362f3ba8073863c8651da1a5e494de0893
haroon-ignitehq <hmahmood@ignitehq.io>
2022-06-27 12:06:43 +0500

    php storm integrated

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PermissionController;
-use App\Http\Controllers\RoleController;
+use App\Http\Controllers\{RoleController, PermissionController, HomeController};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,27 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'welcome' );
+} );
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::get('/logout', [HomeController::class, 'logout'])->name('logout');
+Route::group( [ 'middleware' => 'auth' ], function () {
+    Route::get( '/home', [ HomeController::class, 'index' ] )->name( 'home' );
+    Route::get( '/logout', [ HomeController::class, 'logout' ] )->name( 'logout' );
 
-    Route::group(['prefix' => 'roles', 'as' => 'roles.'], function () {
-        Route::get('/', [RoleController::class, 'index'])->name('index');
-        Route::get('/create', [RoleController::class, 'create'])->name('create');
-        Route::post('/store', [RoleController::class, 'store'])->name('store');
+    Route::group( [ 'prefix' => 'roles', 'as' => 'roles.' ], function () {
+        Route::get( '/', [ RoleController::class, 'index' ] )->name( 'index' );
+        Route::get( '/create', [ RoleController::class, 'create' ] )->name( 'create' );
+        Route::post( '/store', [ RoleController::class, 'store' ] )->name( 'store' );
 
-        Route::group(['prefix' => '/{role_id}'], function () {
-            Route::get('/permissions/{permission_id}/assign', [RoleController::class, 'assignPermission'])->name('assignPermission');
-        });
-    });
+        Route::group( [ 'prefix' => '/{role_id}' ], function () {
+            Route::get( '/permissions/{permission_id}/assign', [ RoleController::class, 'assignPermission' ] )->name( 'assignPermission' );
+        } );
+    } );
 
-    Route::group(['prefix' => 'permissions', 'as' => 'permissions.'], function () {
-        Route::get('/', [PermissionController::class, 'index'])->name('index');
-        Route::get('/create', [PermissionController::class, 'create'])->name('create');
-        Route::post('/store', [PermissionController::class, 'store'])->name('store');
-    });
-});
+    Route::group( [ 'prefix' => 'permissions', 'as' => 'permissions.' ], function () {
+        Route::get( '/', [ PermissionController::class, 'index' ] )->name( 'index' );
+        Route::get( '/create', [ PermissionController::class, 'create' ] )->name( 'create' );
+        Route::post( '/store', [ PermissionController::class, 'store' ] )->name( 'store' );
+    } );
+} );
